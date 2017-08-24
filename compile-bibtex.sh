2fc756efe524a48a9e9e8d48f2c6149053cf546d
Rubens Figueiredo <r.figueiredo.52@gmail.com>
2017-08-24 17:24:13 +0100

    finalize travis integration

--- compile-bibtex.sh
+++ compile-bibtex.sh
@@ -7,6 +7,9 @@ rm *.pdf
 
 pdflatex -interaction=nonstopmode -output-directory . main.tex 
 bibtex main.tex
-pdflatex -interaction=nonstopmode -output-directory . main.tex
+pdflatex -interaction=nonstopmode -output-directory .. main.tex
 
 make clean
+
+cd ..
+rm *.log *.aux *.toc
